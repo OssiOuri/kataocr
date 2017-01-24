d5bba5099ddc96d8acd795b78030bfd14f20f185
Ossi Ouri <ossi.ouri@gmail.com>
2017-01-24 20:38:05 +0200

    typo in path

--- docker/ocr/docker_start_ocr.sh
+++ docker/ocr/docker_start_ocr.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo docker run -P -v /home/jenkins/ci/workspace/pipefirst:opt/share --network=host --name ocrbuntu ocrbuntu
+sudo docker run -P -v /home/jenkins/ci/workspace/pipefirst:/opt/share --network=host --name ocrbuntu ocrbuntu
 sudo docker rm ocrbuntu
