780a2258cee64b2553abf7b9529f7de99bfa81c5
Ossi Ouri <ossi.ouri@gmail.com>
2017-01-24 14:20:11 +0200

    sudo command not found fixed

--- docker/ocr/docker_start_ocr.sh
+++ docker/ocr/docker_start_ocr.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo docker run -P -v /var/jenkins_home/workspace/pipefirst:/opt/share --network=host --name ocrbuntu ocrbuntu
-sudo docker rm ocrbuntu
+docker run -P -v /var/jenkins_home/workspace/pipefirst:/opt/share --network=host --name ocrbuntu ocrbuntu
+docker rm ocrbuntu
