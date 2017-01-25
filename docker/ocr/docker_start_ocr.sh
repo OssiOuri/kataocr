edc09dd715d49cf52f47fd1be2aadf240188ae6e
Ossi Ouri <ossi.ouri@gmail.com>
2017-01-25 09:25:01 +0200

    fixing ci build status when robot tests fails

--- docker/ocr/docker_start_ocr.sh
+++ docker/ocr/docker_start_ocr.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 sudo docker run -P -v /home/jenkins/ci/workspace/pipefirst:/opt/share --network=host --name ocrbuntu ocrbuntu
+return_value=$?
 sudo docker rm ocrbuntu
+exit $return_value
--- docker/ocr/include/a.sh
+++ docker/ocr/include/a.sh
@@ -4,16 +4,12 @@ tar xfvz /tmp/geckodriver.tar.gz
 ln -fs /tmp/geckodriver /usr/bin/geckodriver
 #unzip /tmp/chromedriver.zip -d /tmp
 #cp /tmp/chromedriver /usr/bin/chromedriver
-cd /opt/share/test
-pwd
-ls -l
-./exec_atest.sh
-#if [ $? -eq 0 ]
-#then
-#  echo "Successfully run template"
-#  exit 0
-#else
-#  echo "Unsuccesfully run template" 
-#  exit 1
-#fi
-#/bin/bash
+/opt/share/test/exec_atest.sh
+if [ $? -eq 0 ]
+then
+  echo "Successfully run exec_atest"
+  exit 0
+else
+  echo "Unsuccesfully run exec_atest" 
+  exit 1
+fi
